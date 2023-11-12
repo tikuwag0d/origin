0648936cea6fed264e9c7e1b077c16427028d3c3
Aka <e235758@ie.u-ryukyu.ac.jp>
2023-11-12 16:03:35 +0900

    second commit

--- app/src/test/java/jp/ac/uryukyu/ie/e235758/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e235758/EnemyTest.java
@@ -18,7 +18,11 @@ class EnemyTest {
         Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
         Enemy slime = new Enemy("スライムもどき", 10, 100);
         demoHero.attack(slime);
+            if (slime.hitPoint< 0){
+                return;
+            }
         slime.attack(demoHero);
+
         assertEquals(defaultHeroHp, demoHero.hitPoint);
     }
 }
