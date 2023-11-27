6b9ce9a172de19729648f3bfc20a9fe0b3d9614c
Aka <e235758@ie.u-ryukyu.ac.jp>
2023-11-27 12:16:15 +0900

    report4 0

--- app/src/main/java/jp/ac/uryukyu/ie/e235758/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235758/Enemy.java
@@ -57,12 +57,10 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        if (getHitPoint()< 0){
-            return;
-        }
-
+        if ((this.hitPoint -= damage)>0){
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+        }
     }
 
     /**
